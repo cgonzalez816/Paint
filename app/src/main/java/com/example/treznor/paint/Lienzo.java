ae6c3ffa2a5bfb7a6feda1119a15f5a75fc3f1c5
cesar <cgonzalez816@gmail.com>
2016-10-20 17:07:08 -0600

    adelanto3
    adelanto3

---
+++ app/src/main/java/com/example/treznor/paint/Lienzo.java
@@ -0,0 +1,15 @@
+package com.example.treznor.paint;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by treznor on 20/10/2016.
+ */
+public class Lienzo extends View {
+
+    public Lienzo(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
