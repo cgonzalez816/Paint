603941b00dbef81339781fc88bfa2327e6206e85
cesar <cgonzalez816@gmail.com>
2016-10-22 12:42:41 -0600

    ade3ant6

--- app/src/main/java/com/example/treznor/paint/MainActivity.java
+++ app/src/main/java/com/example/treznor/paint/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.treznor.paint;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -19,7 +20,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     ImageButton azul;
 
     Lienzo lienzo;
+    float ppeque;
+    float pmedia;
+    float pgran;
+    float pdefecto;
 
+    ImageButton trazo;
+    ImageButton ADD;
+    ImageButton borrar;
+    ImageButton guardar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,15 +43,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         verde = (ImageButton)findViewById(R.id.cverde);
         azul = (ImageButton)findViewById(R.id.cazul);
 
+        trazo = (ImageButton)findViewById(R.id.trazo);
+        ADD = (ImageButton)findViewById(R.id.ADD);
+        borrar = (ImageButton)findViewById(R.id.borrar);
+        guardar = (ImageButton)findViewById(R.id.guardar);
+
         negro.setOnClickListener(this);
         blanco.setOnClickListener(this);
         rojo.setOnClickListener(this);
         verde.setOnClickListener(this);
         azul.setOnClickListener(this);
 
+        trazo.setOnClickListener(this);
+        ADD.setOnClickListener(this);
+        borrar.setOnClickListener(this);
+        guardar.setOnClickListener(this);
+
+
 
 
         lienzo = (Lienzo)findViewById(R.id.Lienzo);
+
+        ppeque = 10;
+        pmedia = 20;
+        pgran = 30;
+        pdefecto = pmedia;
 
     }
 
@@ -106,7 +131,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
                 break;
-        default:
+
+            case R.id.trazo:
+                final Dialog tamanyo_punto = new Dialog(this);
+                tamanyo_punto.setTitle("tamaño del punto:");
+
+                tamanyo_punto.setContentView(R.layout.tamanyo_punto);
+
+
+                break;
+
+            case R.id.ADD:
+
+                break;
+
+            case R.id.borrar:
+
+                break;
+
+            case R.id.guardar:
+
+                break;
+
+
+            default:
                 break;
 
         }
